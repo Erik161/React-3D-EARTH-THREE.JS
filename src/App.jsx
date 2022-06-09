3acf63905390af72cb2c7b33241c871d6ddee94e
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-06-08 23:24:00 -0600

    add setup and installing Dependencies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from "styled-components";
+
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+
+function App() {
+
+  return <CanvasContainer>Hello Three</CanvasContainer>
+}
+
+export default App;
