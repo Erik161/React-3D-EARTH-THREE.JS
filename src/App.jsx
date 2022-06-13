e202c44d92fed66e82e8aa7a9fd8337952ba5f75
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-06-12 18:06:30 -0600

    add Let´s Create a simple landing page with our Ginatic rotating earth

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 import { Earth } from './components/earth';
+import {TopSection} from './components/topSection';
+
 const CanvasContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -12,6 +14,7 @@ const CanvasContainer = styled.div`
 function App() {
 
   return <CanvasContainer>
+            <TopSection/>
             <Canvas>
               <Suspense fallback={null}>
                 <Earth/>
